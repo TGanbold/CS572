1d0ab2b5d144f3d08cf4af5d0dde3b759b7e504a
tsoogii <tsogzolmaa.g@gmail.com>
2019-04-11 18:08:02 -0500

    Create exercise1.js

---
+++ Assignment9/exercise1.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const client=new MongoClient('mongodb+srv://test:test@cluster0-fospc.gcp.mongodb.net/test?retryWrites=true'), { useNewUrlParser: true });  
+
+let db;
+let zips;
+
+client.connect(err => {
+  db = client.db('CS572');
+  zips = db.collection('zipcodes'); 
+
+});
+
+app.listen(3000,()=>console.log('Listening on 3000'));
+
+// by STATE
+app.get('/query1', function(req,res){   
+  zips.aggregate([{$match:{state:'IA'}}]).limit(20).toArray().then(
+    result=>  {
+      res.json(result);
+      res.end();
+    }
+    );
+});
+
+// by STATE population less than 5000
+app.get('/query2', function(req,res){   
+  zips.aggregate([{$match:{pop:{$lt:5000}}}])
+  .limit(20)
+  .toArray().then(
+    result=>  {
+      res.json(result);
+      res.end();
+    }
+    );
+});
+
+// by more than one zip
+app.get('/query3', function(req,res){   
+    zips.aggregate([
+    {$group:{_id: {"state":"$state","city":"$city"}
+    ,zipcount:{$sum:1}}}
+    ,{$match: {zipcount:{$gt:1}}}
+    ,{$sort: {state:1,city:1}}])
+  .toArray().then(
+    result=>  {
+      res.json(result);
+      res.end();
+    }
+    );
+});
+
+// by STATE least populated city
+app.get('/query4', function(req,res){   
+  zips.aggregate([
+    {$group: {_id: {"state":"$state","city":"$city"}
+    ,pop:{$sum:"$pop"}}},
+    {$sort: {state:1,pop:-1}},
+    {$group: {_id: {"state":"$_id.state"},city:{$first: "$_id.city"}
+     ,pop:{$first:"$pop"}}}
+  ])
+  .toArray().then(
+    result=>  {
+      res.json(result);
+      res.end();
+    }
+    );
+});
+
